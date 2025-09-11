9d00467ade94b6617e0381a6b629865c13bca003
wzhan246 <wzhan246@asu.edu>
2025-09-11 16:03:19 -0700

    Flask Templates Exercise
    Flask Templates Exercise

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
